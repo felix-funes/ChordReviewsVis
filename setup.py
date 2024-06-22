1be9296238771ac94ceb8364f7db3908c218ae17
Felix Funes <113827328+felix-funes@users.noreply.github.com>
2024-06-22 13:05:02 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 # Package meta-data.
 NAME = "ChordReviewsVis"
-URL = "http://github.com/feature-engine/feature_engine"
+URL = "https://github.com/felix-funes/ChordReviewsVis"
 AUTHOR = "Félix José Funes"
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='ChordReviewsVis',
-    version='0.2.6',
+    version='0.2.7',
     description="Process reviews data, apply text preprocessing, and generate a chord plot visualization showing word co-occurrence patterns and sentiment analysis.",
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'pandas',
         'numpy',
@@ -20,4 +26,8 @@ setup(
         'holoviews'
     ],
     keywords=['customer reviews', 'sentiment analysis', 'chord plot'],
+    url="https://github.com/felix-funes/ChordReviewsVis",
+    project_urls = {
+        'GitHub': 'https://github.com/felix-funes/ChordReviewsVis'
+    }
 )
